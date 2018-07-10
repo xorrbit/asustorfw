82be79f419aea3ffd6c7170b9d49a2732a118b91
Andrew Orr <andrew@andreworr.ca>
2018-07-10 15:56:27 +0000

    Added extracting root filesystem from builtin and initramfs to rootfs dir.

--- asustor-extract.sh
+++ asustor-extract.sh
@@ -24,8 +24,17 @@ header_size=$(($(head -n20 ${firmware} | grep 'body_skip' | cut -d'=' -f2) - 1))
 head -n${header_size} ${firmware} > ${outdir}/header.sh
 offset=$(($(wc -c < ${outdir}/header.sh)))
 
-dd if=${firmware} of=${outdir}/body.tar ibs=${offset} skip=1
+echo "Extracting body..."
+dd status=none if=${firmware} of=${outdir}/body.tar ibs=${offset} skip=1
 cd ${outdir}
 tar -xf body.tar
-#rm body.tar
-#cd ..
+mkdir rootfs
+mv initramfs initramfs.xz
+echo "Extracting initramfs to rootfs ..."
+unxz initramfs.xz
+cd rootfs
+cpio -i --quiet < ../initramfs
+cd ..
+echo "Extracting builtin to rootfs ..."
+tar -xC rootfs -f builtin.tgz
+cd ..
